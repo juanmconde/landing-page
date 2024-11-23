eabc0303d7003aa89e099c48d42c0279ad79b2f9
juanmconde <juanmconde@outlook.com>
2024-11-23 16:28:59 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,10 @@
 const rainContainer = document.querySelector('.rain');
 
-
 for (let i = 0; i < 50; i++) {
   const drop = document.createElement('div');
   drop.classList.add('drop');
 
-  
   drop.style.left = Math.random() * 100 + 'vw';
-
-  
-  drop.style.width = drop.style.height = Math.random() * 20 + 30 + 'px';
-  drop.style.animationDuration = Math.random() * 3 + 2 + 's'; // 2 a 5 segundos
-
+  drop.style.animationDuration = Math.random() * 3 + 2 + 's';
   rainContainer.appendChild(drop);
 }
